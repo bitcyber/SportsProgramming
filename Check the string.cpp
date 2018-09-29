9bc74726af88483fcdc8690367d500e9bac205c2
Amit <amit2rockpower@gmail.com>
2018-09-30 02:02:00 +0530

    check the string

--- Check the string.cpp
+++ Check the string.cpp
@@ -6,3 +6,21 @@ using namespace std ;
 #define ll long long int
 #define endl "\n"
 #define REP(i,n) for(int i=0;i<n;i++)
+
+int main()
+{
+    speedup;
+    string ss;
+    getline(cin,ss);
+    // cin.ignore();
+    // cout<<ss<<endl;
+    string xx;
+    xx = ss;
+    sort(xx.begin(),xx.end());
+    // cout<<ss<<" "<<xx<<endl;
+    int count_a=0,count_b=0,count_c=0;
+    count_a = count(xx.begin(),xx.end(),'a');
+    count_b = count(xx.begin(),xx.end(),'b');
+    count_c = count(xx.begin(),xx.end(),'c');
+    (ss==xx&&(count_c ==count_a || count_c == count_b) && count_a>=1 && count_b>=1)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+}
