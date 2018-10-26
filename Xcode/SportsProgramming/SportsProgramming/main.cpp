f2029a30b5576197e752098df67a749825d7c540
Amit <amit2rockpower@gmail.com>
2018-10-26 08:53:07 +0530

    xcode initialising

---
+++ Xcode/SportsProgramming/SportsProgramming/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SportsProgramming
+//
+//  Created by amit on 26/10/18.
+//  Copyright © 2018 amit2rockon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Xcode/SportsProgramming/SportsProgramming/test.cpp
@@ -0,0 +1,9 @@
+//
+//  test.cpp
+//  SportsProgramming
+//
+//  Created by amit on 26/10/18.
+//  Copyright © 2018 amit2rockon. All rights reserved.
+//
+
+#include <stdio.h>
